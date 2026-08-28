532f7963ea352208f8614a704cfff9c10e0da44e
sitharashirin957 <sitharashirin957@gmail.com>
2026-08-28 23:58:47 +0300

    Fixed notification syntax and updated app icons

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: 'class', // <--- ദാ, ഈ ലൈൻ ആണ് നമ്മൾ പുതിയതായി ചേർത്തത്
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
